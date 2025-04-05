0298e1bb194bafecb4eefae1f5238692778f7e68
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-05 22:00:45 +0000

    bugfix and stuff

--- init-traces.sh
+++ init-traces.sh
@@ -1,21 +1,25 @@
 set -eux
 
-rm -f traces*.txt
+PROC_MAPS_FILE=proc_maps.txt
+OUTPUT_FILE=processed_traces.txt
+
+rm -f traces*.txt $PROC_MAPS_FILE $OUTPUT_FILE
 
 make -j7 CXXFLAGS_EXTRA="-O0 -ggdb3 -finstrument-functions -finstrument-functions-exclude-file-list=src/tracing.cpp,/usr/lib,/usr/include,stl_tree,strl_map,stl_vector -Wl,-Map=asprof.map"
 javac MyMain.java
 
-java -agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=100ms MyMain &
+ASPROF_CMD=-agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=100ms
+java $ASPROF_CMD MyMain > /dev/null &
 sleep 20
 
 JPID=$(jps | grep MyMain | awk '{print $1}')
 if [ -z "$JPID" ]; then exit 1; fi
 
+trap "kill -9 $JPID || true" EXIT
+
 BASE_ADDRESS=$(cat /proc/$JPID/maps | grep profiler | awk '{print $1}' | awk -F  '-' '{print $1}' | head -n 1)
 echo $BASE_ADDRESS > base_address.txt
-cat /proc/$JPID/maps > proc_maps.txt
+cat /proc/$JPID/maps > $PROC_MAPS_FILE
 
 kill -SIGTERM $JPID
-sleep 2
-
-python3 process-instrumentation/process.py $BASE_ADDRESS > processed_traces.txt
+python3 process-instrumentation/process.py $PROC_MAPS_FILE "traces*.txt" > $OUTPUT_FILE
--- process-instrumentation/process.py
+++ process-instrumentation/process.py
@@ -14,7 +14,10 @@ from proc_map_utils import (
     read_proc_map,
     find_function_name,
     ProcMap,
+    keep_only_base_address,
 )
+
+last_time: int = -1
 
 
 @functools.lru_cache(maxsize=100_000)
@@ -27,54 +30,58 @@ def _find_function_name(address: str, proc_map: ProcMap) -> typing.Optional[str]
     shifted_address = address_int - load_location.address_start
     if shifted_address < 0:
         raise ValueError(f"{load_location}, {address}")
-    return find_function_name(address=shifted_address, lib_path=load_location.lib_path)
+
+    function_name = find_function_name(
+        address=shifted_address, lib_path=load_location.lib_path
+    )
+    return function_name if function_name else address
 
 
 def _process_line(line: str, last_trie_node: TrieNode, proc_map: ProcMap) -> TrieNode:
-    enter_exit, partial_time_s, partial_time_ns, caller_address, callee_address = (
-        line.split(",")
-    )
+    enter_exit, time_s, caller_address, callee_address = line.split(",")
 
-    time_ns = int(partial_time_ns) + int(partial_time_s) * int(1e9)
+    time = int(time_s)
+    if time < last_time:
+        raise ValueError(
+            f"Expected monotonically increasing time, offending line: {line}"
+        )
 
     caller_name = _find_function_name(address=caller_address, proc_map=proc_map)
-    if not caller_name:
-        caller_name = caller_address
-
     callee_name = _find_function_name(address=callee_address, proc_map=proc_map)
-    if not callee_name:
-        callee_name = callee_address
 
     if enter_exit == "E":
-        if last_trie_node.name != caller_name:
-            warnings.warn(f"Last node is '{last_trie_node.name}', got '{caller_name}'")
+        if not last_trie_node.is_root and last_trie_node.name != caller_name:
+            warnings.warn(
+                f"Last node is '{last_trie_node.name}', got '{caller_name}', line: '{line}'"
+            )
+        # Let's just assume that the address points to a different line in the same function
 
         if callee_name in last_trie_node.children:
-            if last_trie_node.children[callee_name].last_entry_time_ns != -1:
-                raise RuntimeError(f"last_entry_time_ns for {callee_name} is not -1")
+            if last_trie_node.children[callee_name].last_entry_time != -1:
+                raise RuntimeError(f"last_entry_time for {callee_name} is not -1")
         else:
             last_trie_node.children[callee_name] = TrieNode(
                 name=callee_name, parent=last_trie_node
             )
 
-        last_trie_node.children[callee_name].last_entry_time_ns = time_ns
-        return last_trie_node.children[callee_name]
+        node = last_trie_node.children[callee_name]
+        node.last_entry_time = time
+        return node
 
-    if enter_exit == "X":
+    elif enter_exit == "X":
         if last_trie_node.name != callee_name:
-            warnings.warn(
-                f"The active node is {last_trie_node.name}, not {callee_name}, ignoring"
+            raise RuntimeError(
+                f"The current trie node is {last_trie_node.name}, not {callee_name}"
             )
-            return
 
-        if last_trie_node.last_entry_time_ns == -1:
-            raise RuntimeError(f"last_entry_time_ns for {callee_name} is -1")
+        if last_trie_node.last_entry_time == -1:
+            raise RuntimeError(f"last_entry_time for {callee_name} is -1")
 
-        if last_trie_node.parent is None:
+        if last_trie_node.is_root:
             raise RuntimeError(f"Can't exit from root")
 
-        last_trie_node.time_ns = time_ns - last_trie_node.last_entry_time_ns
-        last_trie_node.last_entry_time_ns = -1
+        last_trie_node.time_total += time - last_trie_node.last_entry_time
+        last_trie_node.last_entry_time = -1
         return last_trie_node.parent
 
     raise RuntimeError(f"Unexpected value for enter_exit={enter_exit}")
@@ -82,11 +89,20 @@ def _process_line(line: str, last_trie_node: TrieNode, proc_map: ProcMap) -> Tri
 
 def _process_path(path: pathlib.Path, proc_map: ProcMap) -> TrieNode:
     root = last_trie_node = TrieNode(name=path.name)
+
     with open(path) as file:
         for line in file:
             last_trie_node = _process_line(
                 line=line.strip(), last_trie_node=last_trie_node, proc_map=proc_map
             )
+
+    if last_trie_node is not root:
+        raise RuntimeError(
+            f"The stack for {path} did not come back to the root: {last_trie_node}"
+        )
+
+    for child in root.children.values():
+        root.time_total += child.time_total
 
     return root
 
@@ -97,21 +113,22 @@ def _dfs(node: TrieNode, tree: list[str]) -> int:
     for child in node.children.values():
         children_duration_ns += _dfs(child, tree)
 
-    print(f"{';'.join(tree)} {node.time_ns - children_duration_ns}")
-
+    print(f"{';'.join(tree)} {node.time_total - children_duration_ns}")
     tree.pop()
 
-    non_cum_time = node.time_ns - children_duration_ns
+    non_cum_time = node.time_total - children_duration_ns
     if non_cum_time < 0:
         warnings.warn(f"Non-cumulative time for {node.name} is negative")
+        return 0
+
     return non_cum_time
 
 
 if __name__ == "__main__":
-    proc_map = read_proc_map(sys.argv[1])
+    proc_map = keep_only_base_address(read_proc_map(sys.argv[1]))
 
     with multiprocessing.Pool(processes=4) as pool:
-        paths = tuple(pathlib.Path(".").glob("traces*.txt"))
+        paths = pathlib.Path(".").glob(sys.argv[2])
         roots = pool.starmap(_process_path, ((p, proc_map) for p in paths))
 
     for root_node in roots:
