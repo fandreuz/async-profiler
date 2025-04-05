ffb4a49defcbc26ce3a9fbd749a2251d7b89fbcc
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-05 22:00:17 +0000

    bugfix

--- process-instrumentation/force_find_address.py
+++ process-instrumentation/force_find_address.py
@@ -5,11 +5,13 @@ sys.path.append(str(pathlib.Path(__file__).parent))
 from proc_map_utils import find_function_name, read_proc_map
 
 if __name__ == "__main__":
-    _address = sys.argv[1]
+    _address = int(sys.argv[1], base=16)
     proc_map = read_proc_map(pathlib.Path(sys.argv[2]))
     for address_start, lib_path in zip(proc_map.address_starts, proc_map.lib_paths):
-        if ".so" in lib_path:
-            name = find_function_name(
-                address=_address, base_address_int=address_start, lib_path=lib_path
-            )
-            print(f"{lib_path} -> {name}")
+        shifted_address = _address - address_start
+        if shifted_address > 0 and lib_path and pathlib.Path(lib_path).exists():
+            try:
+                name = find_function_name(address=shifted_address, lib_path=lib_path)
+                print(f"{lib_path} -> {name}")
+            except Exception as e:
+                print(f"{lib_path} -> {e}")
