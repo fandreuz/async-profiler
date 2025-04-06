5bad1d3327cde502764bb8d8daf4220ef724fc74
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-06 10:40:39 +0000

    allow setting asprof_cmd

--- init-traces.sh
+++ init-traces.sh
@@ -2,13 +2,13 @@ set -eux
 
 OUTPUT_FILE=${OUTPUT_FILE:-processed_traces.txt}
 PROC_MAPS_FILE=proc_maps.txt
+ASPROF_CMD=${ASPROF_CMD:--agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms}
 
 rm -f traces*.txt $PROC_MAPS_FILE $OUTPUT_FILE
 
 make -j7 CXXFLAGS_EXTRA="-O0 -ggdb3 -finstrument-functions -finstrument-functions-exclude-file-list=src/tracing.cpp,/usr/lib,/usr/include,stl_tree,strl_map,stl_vector -Wl,-Map=asprof.map"
 javac MyMain.java
 
-ASPROF_CMD=-agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms
 PROC_MAPS_COPY_PATH=$PROC_MAPS_FILE java $ASPROF_CMD MyMain > /dev/null &
 sleep 20
 
--- instrument-branches.sh
+++ instrument-branches.sh
@@ -1,6 +1,7 @@
 set -eux
 
-original_branch=$(git rev-parse --abbrev-ref HEAD)
+ORIGINAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+ASPROF_CMD=${ASPROF_CMD:--agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms}
 
 git checkout $1
 git checkout -b tmp-$1
@@ -8,7 +9,7 @@ trap "git branch -D tmp-$1" EXIT
 git merge --no-edit instrumenting-asprof
 
 OUTPUT_FILE_1=processed_traces_$1.txt
-OUTPUT_FILE=$OUTPUT_FILE_1 ./init-traces.sh
+OUTPUT_FILE=$OUTPUT_FILE_1 ASPROF_CMD=$ASPROF_CMD ./init-traces.sh
 
 git checkout $2
 git checkout -b tmp-$2
@@ -16,10 +17,10 @@ trap "git branch -D tmp-$1 && git branch -D tmp-$2" EXIT
 git merge --no-edit instrumenting-asprof
 
 OUTPUT_FILE_2=processed_traces_$2.txt
-OUTPUT_FILE=$OUTPUT_FILE_2 ./init-traces.sh
+OUTPUT_FILE=$OUTPUT_FILE_2 ASPROF_CMD=$ASPROF_CMD ./init-traces.sh
 
 DIFF_OUTPUT_FILE=diff_$1_$2.txt
 python3 process-instrumentation/diff.py $OUTPUT_FILE_1 $OUTPUT_FILE_2 > $DIFF_OUTPUT_FILE
 ../FlameGraph/flamegraph.pl $DIFF_OUTPUT_FILE > diff_$1_$2.svg
 
-git checkout $original_branch
+git checkout $ORIGINAL_BRANCH
