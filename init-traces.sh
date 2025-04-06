8f28a96a5b128cb45c0b97a124b6416f3f070329
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-06 09:23:34 +0000

    interval and aggregation

--- init-traces.sh
+++ init-traces.sh
@@ -8,7 +8,7 @@ rm -f traces*.txt $PROC_MAPS_FILE $OUTPUT_FILE
 make -j7 CXXFLAGS_EXTRA="-O0 -ggdb3 -finstrument-functions -finstrument-functions-exclude-file-list=src/tracing.cpp,/usr/lib,/usr/include,stl_tree,strl_map,stl_vector -Wl,-Map=asprof.map"
 javac MyMain.java
 
-ASPROF_CMD=-agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=100ms
+ASPROF_CMD=-agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms
 PROC_MAPS_COPY_PATH=$PROC_MAPS_FILE java $ASPROF_CMD MyMain > /dev/null &
 sleep 20
 
@@ -18,4 +18,4 @@ if [ -z "$JPID" ]; then exit 1; fi
 trap "kill -9 $JPID || true" EXIT
 kill -SIGTERM $JPID
 
-python3 process-instrumentation/process.py $PROC_MAPS_FILE "traces*.txt" > $OUTPUT_FILE
+python3 process-instrumentation/process.py $PROC_MAPS_FILE "traces*.txt" true > $OUTPUT_FILE
