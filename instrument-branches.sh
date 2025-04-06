cb04adfcaa4f80332ecb0d52fc673a70d5e8e705
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-06 10:46:31 +0000

    ASPROF_INSTRUMENTATION_BRANCH

--- instrument-branches.sh
+++ instrument-branches.sh
@@ -2,11 +2,12 @@ set -eux
 
 ORIGINAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 ASPROF_CMD=${ASPROF_CMD:--agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms}
+ASPROF_INSTRUMENTATION_BRANCH=instrumenting-asprof
 
 git checkout $LEFT
 git checkout -b tmp-$LEFT
 trap "git branch -D tmp-$LEFT" EXIT
-git merge --no-edit instrumenting-asprof
+git merge --no-edit $ASPROF_INSTRUMENTATION_BRANCH
 
 OUTPUT_FILE_1=processed_traces_${LEFT}.txt
 OUTPUT_FILE=$OUTPUT_FILE_1 ASPROF_CMD=$ASPROF_CMD ./init-traces.sh
@@ -14,10 +15,12 @@ OUTPUT_FILE=$OUTPUT_FILE_1 ASPROF_CMD=$ASPROF_CMD ./init-traces.sh
 git checkout $RIGHT
 git checkout -b tmp-$RIGHT
 trap "git branch -D tmp-$LEFT && git branch -D tmp-$RIGHT" EXIT
-git merge --no-edit instrumenting-asprof
+git merge --no-edit $ASPROF_INSTRUMENTATION_BRANCH
 
 OUTPUT_FILE_2=processed_traces_${RIGHT}.txt
 OUTPUT_FILE=$OUTPUT_FILE_2 ASPROF_CMD=$ASPROF_CMD ./init-traces.sh
+
+git checkout $ASPROF_INSTRUMENTATION_BRANCH
 
 DIFF_OUTPUT_FILE=diff_${LEFT}_${RIGHT}.txt
 python3 process-instrumentation/diff.py $OUTPUT_FILE_1 $OUTPUT_FILE_2 > $DIFF_OUTPUT_FILE
