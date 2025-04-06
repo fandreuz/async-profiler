05e5ba222b7359cadef863b6d8185fbff31cef0c
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-06 07:35:13 +0000

    fmt

--- init-traces.sh
+++ init-traces.sh
@@ -16,6 +16,6 @@ JPID=$(jps | grep MyMain | awk '{print $1}')
 if [ -z "$JPID" ]; then exit 1; fi
 
 trap "kill -9 $JPID || true" EXIT
-
 kill -SIGTERM $JPID
+
 python3 process-instrumentation/process.py $PROC_MAPS_FILE "traces*.txt" > $OUTPUT_FILE
