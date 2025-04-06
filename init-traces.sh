9a94fba1bd363c32ac97febe6038c42f76c87d0b
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-06 19:56:52 +0000

    quote

--- init-traces.sh
+++ init-traces.sh
@@ -8,7 +8,7 @@ rm -f traces*.txt $PROC_MAPS_FILE $OUTPUT_FILE
 rm -rf build
 
 CXXFLAGS_EXTRA="-O0 -ggdb3 -finstrument-functions -finstrument-functions-exclude-file-list=src/tracing.cpp,/usr/lib,/usr/include,stl_tree,strl_map,stl_vector"
-make -j CXXFLAGS_EXTRA=$CXXFLAGS_EXTRA
+make -j CXXFLAGS_EXTRA="$CXXFLAGS_EXTRA"
 javac MyMain.java
 
 PROC_MAPS_COPY_PATH=$PROC_MAPS_FILE java $ASPROF_CMD MyMain > /dev/null 2> ${OUTPUT_FILE}.err &
