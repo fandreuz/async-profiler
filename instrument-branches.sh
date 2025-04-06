0bfdb79a0a23343ec4ce3d90e581abe1fb1f4853
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-06 10:54:50 +0000

    wrap

--- instrument-branches.sh
+++ instrument-branches.sh
@@ -8,7 +8,7 @@ git checkout -b tmp-$LEFT
 trap "git branch -D tmp-$LEFT" EXIT
 git merge --no-edit instrumenting-asprof
 
-OUTPUT_FILE_1=processed_traces_$LEFT.txt
+OUTPUT_FILE_1=processed_traces_${LEFT}.txt
 OUTPUT_FILE=$OUTPUT_FILE_1 ASPROF_CMD=$ASPROF_CMD ./init-traces.sh
 
 git checkout $RIGHT
@@ -16,11 +16,11 @@ git checkout -b tmp-$RIGHT
 trap "git branch -D tmp-$LEFT && git branch -D tmp-$RIGHT" EXIT
 git merge --no-edit instrumenting-asprof
 
-OUTPUT_FILE_2=processed_traces_$RIGHT.txt
+OUTPUT_FILE_2=processed_traces_${RIGHT}.txt
 OUTPUT_FILE=$OUTPUT_FILE_2 ASPROF_CMD=$ASPROF_CMD ./init-traces.sh
 
-DIFF_OUTPUT_FILE=diff_$LEFT_$RIGHT.txt
+DIFF_OUTPUT_FILE=diff_${LEFT}_${RIGHT}.txt
 python3 process-instrumentation/diff.py $OUTPUT_FILE_1 $OUTPUT_FILE_2 > $DIFF_OUTPUT_FILE
-../FlameGraph/flamegraph.pl $DIFF_OUTPUT_FILE > diff_$LEFT_$RIGHT.svg
+../FlameGraph/flamegraph.pl $DIFF_OUTPUT_FILE > diff_${LEFT}_${RIGHT}.svg
 
 git checkout $ORIGINAL_BRANCH
