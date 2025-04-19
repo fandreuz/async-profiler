e755a7021969a772a8b590425036ab7317d4a1d5
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-19 21:02:29 +0000

    new params

--- init-traces.sh
+++ init-traces.sh
@@ -3,15 +3,16 @@ set -eux
 OUTPUT_FILE=${OUTPUT_FILE:-processed_traces.txt}
 PROC_MAPS_FILE=proc_maps.txt
 ASPROF_CMD=${ASPROF_CMD:--agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms}
+WAIT_TIME_S=${WAIT_TIME_S:-20}
+THREADS_COUNT=${THREADS_COUNT:-5}
 
-rm -f traces*.txt $PROC_MAPS_FILE $OUTPUT_FILE
-rm -rf build
+rm -rf build traces*.txt $PROC_MAPS_FILE $OUTPUT_FILE
 
 CXXFLAGS_EXTRA="-fplugin=./instrument-attribute-gcc-plugin/instrument_attribute.so -finstrument-functions -O0 -ggdb3"
 make -j CXXFLAGS_EXTRA="$CXXFLAGS_EXTRA"
 javac MyMain.java
 
-PROC_MAPS_COPY_PATH=$PROC_MAPS_FILE java $ASPROF_CMD MyMain > /dev/null 2> ${OUTPUT_FILE}.err &
+PROC_MAPS_COPY_PATH=$PROC_MAPS_FILE java $ASPROF_CMD MyMain $THREADS_COUNT $WAIT_TIME_S > /dev/null 2> ${OUTPUT_FILE}.err &
 sleep 20
 
 python3 process-instrumentation/process_trees.py $PROC_MAPS_FILE "traces*.txt" > $OUTPUT_FILE
