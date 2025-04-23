ad20e5f4263d59d8ddedde9e1c1cb48aacaa7e61
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-23 13:57:36 +0000

    dumb

--- init-traces.sh
+++ init-traces.sh
@@ -1,7 +1,7 @@
 set -eux
 
 OUTPUT_FILE=${OUTPUT_FILE:-processed_traces.txt}
-ASPROF_CMD=${ASPROF_CMD:--agentpath:./build/lib/libasyncProfiler.so=start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms}
+ASPROF_CMD=${ASPROF_CMD:-start,timeout=10,collapsed,file=/dev/null,event=cpu,interval=10ms}
 WAIT_TIME_S=${WAIT_TIME_S:-20}
 THREADS_COUNT=${THREADS_COUNT:-5}
 SAMPLES_COUNT_FILE=${SAMPLES_COUNT_FILE:-samples_count.txt}
@@ -12,7 +12,7 @@ CXXFLAGS_EXTRA="-finstrument-functions -finstrument-functions-exclude-file-list=
 make -j CXXFLAGS_EXTRA="$CXXFLAGS_EXTRA"
 javac MyMain.java
 
-timeout $(($WAIT_TIME_S + 5)) java $ASPROF_CMD MyMain $THREADS_COUNT $WAIT_TIME_S > /dev/null 2> ${OUTPUT_FILE}.err
+timeout $(($WAIT_TIME_S + 5)) java -agentpath:./build/lib/libasyncProfiler.so=$ASPROF_CMD MyMain $THREADS_COUNT $WAIT_TIME_S > /dev/null 2> ${OUTPUT_FILE}.err
 
 samples_count=$(grep "recordSample " traces*.txt | awk '{print $NF}' | awk '{s+=$1} END {print s}' )
 if [ $samples_count -lt 100 ]; then
